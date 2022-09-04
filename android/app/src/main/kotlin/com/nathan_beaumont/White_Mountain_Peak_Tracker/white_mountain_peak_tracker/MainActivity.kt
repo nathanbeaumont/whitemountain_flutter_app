106d8f656a821c3314ccd72dace4a2a4cfc41cc0
Nathan Beaumont <nathan.beaumont@whoop.com>
2022-09-04 11:22:33 -0400

    initial commit

---
+++ android/app/src/main/kotlin/com/nathan_beaumont/White_Mountain_Peak_Tracker/white_mountain_peak_tracker/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.nathan_beaumont.White_Mountain_Peak_Tracker.white_mountain_peak_tracker
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
